f342da7793048e7fae11ae3aac4c43e223e2d13f
Tai An Su <i@taian.su>
2019-12-11 03:57:19 +0800

    Refactor day2 part1

--- 2019/src/day2.rs
+++ 2019/src/day2.rs
@@ -1,26 +1,32 @@
 use crate::loader::vec_from;
 
-pub fn part1()  -> Vec<i32> {
+pub fn part1() -> i32 {
     let mut opcodes = vec_from(module_path!());
+    opcodes[1] = 12;
+    opcodes[2] = 2;
+    computer(opcodes)
+}
+
+fn computer(input: Vec<i32>) -> i32 {
+    let mut memory = input;
     let mut current_ptr = 0;
 
-    while opcodes[current_ptr] != 99 {
-        let fst_ptr = opcodes[current_ptr + 1] as usize;
-        let snd_ptr = opcodes[current_ptr + 2] as usize;
-        let target = opcodes[current_ptr + 3] as usize;
+    #[allow(irrefutable_let_patterns)]
+    while let command = memory[current_ptr] {
+        let fst_ptr = memory[current_ptr + 1] as usize;
+        let snd_ptr = memory[current_ptr + 2] as usize;
+        let target = memory[current_ptr + 3] as usize;
 
-        match opcodes[current_ptr]{
-            1 => {
-                opcodes[target] =  opcodes[fst_ptr] + opcodes[snd_ptr];
-                current_ptr += 4;
-            },
-            2 => {
-                opcodes[target] = opcodes[fst_ptr] * opcodes[snd_ptr];
-                current_ptr += 4;
-            }
-            _ => { break; }
+        if command == 1 {
+            memory[target] =  memory[fst_ptr] + memory[snd_ptr];
+            current_ptr += 4;
+        } else if command == 2 {
+            memory[target] = memory[fst_ptr] * memory[snd_ptr];
+            current_ptr += 4;
+        } else {
+            break;
         }
     }
 
-    opcodes
+    memory[0]
 }
