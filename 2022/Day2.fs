5879b84f3350a99002efe74591bb9245ea220d95
Tai An Su <a@tai.su>
2022-12-03 01:32:27 +0800

    2022 day2

---
+++ 2022/Day2.fs
@@ -0,0 +1,60 @@
+module Aoc.Day2
+
+let data = System.IO.File.ReadAllLines "day2.txt"
+
+let games = data |> Array.map (fun line -> line.Split [| ' ' |])
+
+type game = string array
+
+let match_score1 =
+    function
+    | [| "A"; "Z" |]
+    | [| "B"; "X" |]
+    | [| "C"; "Y" |] -> 0
+    | [| "A"; "X" |]
+    | [| "B"; "Y" |]
+    | [| "C"; "Z" |] -> 3
+    | [| "A"; "Y" |]
+    | [| "B"; "Z" |]
+    | [| "C"; "X" |] -> 6
+    | _ -> 0
+
+let shape_score1 =
+    function
+    | [| _; "X" |] -> 1
+    | [| _; "Y" |] -> 2
+    | [| _; "Z" |] -> 3
+    | _ -> 0
+
+let game_score match_score_fn shape_score_fn (game: game) =
+    match_score_fn game + shape_score_fn game
+
+let first () =
+    games |> Array.map (game_score match_score1 shape_score1) |> Array.sum
+
+let match_score2 =
+    function
+    | [| _; "X" |] -> 0
+    | [| _; "Y" |] -> 3
+    | [| _; "Z" |] -> 6
+    | _ -> 0
+
+let shape_score2 =
+    function
+    | [| "A"; "Y" |]
+    | [| "B"; "X" |]
+    | [| "C"; "Z" |] -> 1
+    | [| "A"; "Z" |]
+    | [| "B"; "Y" |]
+    | [| "C"; "X" |] -> 2
+    | [| "A"; "X" |]
+    | [| "B"; "Z" |]
+    | [| "C"; "Y" |] -> 3
+    | _ -> 0
+
+let second () =
+    games |> Array.map (game_score match_score2 shape_score2) |> Array.sum
+
+
+
+// let run () = printfn "data: %A" score
--- 2022/Program.fs
+++ 2022/Program.fs
@@ -2,3 +2,7 @@
 let day1First = Aoc.Day1.first ()
 let day1Second = Aoc.Day1.second ()
 printfn $"Day1 first; {day1First}; Day1 second: {day1Second}"
+
+let day2First = Aoc.Day2.first ()
+let day2Second = Aoc.Day2.second ()
+printfn $"Day2 first; {day2First}; Day2 second: {day2Second}"
